53ee0c607704ea765ca7a3470e8ef931a1c9cd3f
황두리 <hwangduli0515@gmail.com>
2022-10-22 19:53:29 +0900

    프록시 패턴 예제 코드

--- src/test/java/hello/proxy/pureproxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy;
 
+import hello.proxy.pureproxy.code.CacheProxy;
 import hello.proxy.pureproxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,17 @@ public class ProxyPatternTest {
         client.execute();
         client.execute();
 
+    }
+
+    @Test
+    void cacheProxyTest() {
+
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
     }
 
 }
